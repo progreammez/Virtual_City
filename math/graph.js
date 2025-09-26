e637576bb39b8aaa1f6c5066758e7409d1a9d67d
Aditya Chandra Pant <notbigboi69@gmail.com>
2025-09-26 09:54:37 +0530

    graph.js

---
+++ math/graph.js
@@ -0,0 +1,48 @@
+class Graph{
+    constructor(points = [], segments = []){
+        this.points = points;
+        this.segments = segments;
+    }
+
+    addPoint(point){
+        this.points.push(point);
+    }
+
+    containsPoint(point){
+        return this.points.find((p) => p.equals(point));
+    }
+
+    tryAddPoint(point){
+        if(!this.containsPoint(point)){
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+    addSegment(segment){
+        this.segments.push(segment);
+    }
+
+    containsSegment(segment){
+        return this.segments.find((s) => s.equals(seg));
+    }
+
+    tryAddSegment(segment){
+        if(!this.containsSegment(seg)){
+            this.addSegment(seg);
+            return true;
+        }
+            return false;
+        } 
+
+    draw(ctx){
+        for(const seg of this.segments){
+            seg.draw(ctx);
+        }
+
+        for(const point of this.points){
+            point.draw(ctx);
+        }
+    }
+}
